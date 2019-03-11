8a454e684ecfe6daf6dc38522cdc1f13b78b4ec9
Filip Vušković <vuskovic09@users.noreply.github.com>
2019-03-11 17:45:29 +0100

    MOD: index.html, main.js
    Modified index, 90% done save for some footer stuff

--- js/main.js
+++ js/main.js
@@ -11,6 +11,30 @@ $(document).ready(function() {
       console.log("error");
     }
   });
+
+  $.ajax({
+    url: "data/news.json",
+    type: "json",
+    method: "GET",
+    success: function(news){
+      writeNews(news);
+    },
+    error: function(){
+      console.log("error");
+    }
+  });
+
+  $.ajax({
+    url: "data/products.json",
+    type: "json",
+    method: "GET",
+    success: function(products){
+      writeProducts(products);
+    },
+    error: function(){
+      console.log("error");
+    }
+  });
 });
 
 function writeTrending(products){
@@ -21,6 +45,32 @@ function writeTrending(products){
     }
   }
   $('.trendingProducts').html(html);
+}
+
+function writeNews(news){
+  let html = "";
+  for(let article of news){
+    html += writeOneArticle(article);
+  }
+  $('.news').html(html);
+}
+
+function writeOneArticle(article){
+  return `<div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="card card-blog">
+              <div class="card-blog__img">
+                <img class="card-img rounded-0" src="${article.img.src}" alt="${article.img.alt}">
+              </div>
+              <div class="card-body">
+                <ul class="card-blog__info">
+                  <li>${article.author}</li>
+                  <li><i class="ti-comments-smiley"></i>${article.commentNo} Comments</li>
+                </ul>
+                <h4 class="card-blog__title"><a href="#">${article.title}</a></h4>
+                <p>${article.content}</p>
+              </div>
+            </div>
+          </div>`;
 }
 
 function writeProducts(products){
