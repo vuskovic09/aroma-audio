377548a8881fd4019f74bbe8d1a2f85717e92412
Filip Vušković <vuskovic09@users.noreply.github.com>
2019-03-12 02:09:12 +0100

    RM: old jquery & skrollr; ADD: hero-banner & new jquery
    Fixed scrolling

--- js/main.js
+++ js/main.js
@@ -38,6 +38,7 @@ $(document).ready(function() {
 
 });
 
+
 function writeTrending(products){
   let html = "";
   for(let product of products){
@@ -133,40 +134,6 @@ $(function() {
     }
   });
 
-  //------- Best Seller Carousel -------//
-  if($('.owl-carousel').length > 0){
-    $('#bestSellerCarousel').owlCarousel({
-      loop:true,
-      margin:30,
-      nav:true,
-      navText: ["<i class='ti-arrow-left'></i>","<i class='ti-arrow-right'></i>"],
-      dots: false,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items: 2
-        },
-        900:{
-          items:3
-        },
-        1130:{
-          items:4
-        }
-      }
-    })
-  }
-
-  //------- single product area carousel -------//
-  $(".s_Product_carousel").owlCarousel({
-    items:1,
-    autoplay:false,
-    autoplayTimeout: 5000,
-    loop:true,
-    nav:false,
-    dots:false
-  });
 
   //------- mailchimp --------//
 	function mailChimp() {
@@ -183,37 +150,5 @@ $(function() {
     else sticky.removeClass('fixed');
   });
 
-  //------- Price Range slider -------//
-  if(document.getElementById("price-range")){
-
-    var nonLinearSlider = document.getElementById('price-range');
-
-    noUiSlider.create(nonLinearSlider, {
-        connect: true,
-        behaviour: 'tap',
-        start: [ 500, 4000 ],
-        range: {
-            // Starting at 500, step the value by 500,
-            // until 4000 is reached. From there, step by 1000.
-            'min': [ 0 ],
-            '10%': [ 500, 500 ],
-            '50%': [ 4000, 1000 ],
-            'max': [ 10000 ]
-        }
-    });
-
-
-    var nodes = [
-        document.getElementById('lower-value'), // 0
-        document.getElementById('upper-value')  // 1
-    ];
-
-    // Display the slider value and how far the handle moved
-    // from the left edge of the slider.
-    nonLinearSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
-        nodes[handle].innerHTML = values[handle];
-    });
-
-  }
 
 });
